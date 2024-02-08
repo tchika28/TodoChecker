68a39a308cc410d1b5f08ddb243fc33f3fbd4787
chika <chika.us.j24@gmail.com>
2024-02-08 00:28:35 -0800

    update: loop workflow to choose todo actions

--- main.py
+++ main.py
@@ -2,9 +2,19 @@ from ToDoList import ToDoList
 if __name__ == "__main__":
     print("let's to DO!!")
     todo = ToDoList()
-    todo.add_task("洗濯")
-    todo.show_tasks()
-    todo.add_task("洗い物")
-    todo.show_tasks()
-    todo.remove_task("洗い物")
-    todo.show_tasks()
+   
+    while True:
+        print("\nCommands: \n1. Add task\n2. Remove task\n3. Show tasks\n4. Exit")
+        choice = input("Enter choice: ")
+        if choice == '1':
+            task = input("Enter task: ")
+            todo.add_task(task)
+        elif choice == '2':
+            task =input("Enter task to remove: ")
+            todo.remove_task(task)
+        elif choice == '3':
+            todo.show_tasks()
+        elif choice == '4':
+            break
+        else:
+            print("Invalid choice!")
