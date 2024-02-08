1d1f6f9a4f56fb9ab7a05040f60049b2ea9631c3
chika <chika.us.j24@gmail.com>
2024-02-07 23:38:34 -0800

    add: add_task and remove_task function in ToDoList class

--- ToDoList.py
+++ ToDoList.py
@@ -2,6 +2,15 @@ class ToDoList:
     def __init__(self):
         self.tasks = []
 
+    def add_task(self, task):
+        self.tasks.append(task)
+
+    def remove_task(self, task):
+        if task in self.tasks:
+            self.tasks.remove(task)
+        else:
+            print("Tasks not found!")
+
     def show_tasks(self):
         if self.tasks:
             print("Tasks:")
--- main.py
+++ main.py
@@ -2,4 +2,9 @@ from ToDoList import ToDoList
 if __name__ == "__main__":
     print("let's to DO!!")
     todo = ToDoList()
+    todo.add_task("洗濯")
+    todo.show_tasks()
+    todo.add_task("洗い物")
+    todo.show_tasks()
+    todo.remove_task("洗い物")
     todo.show_tasks()
